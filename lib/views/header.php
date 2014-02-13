fd0c9a912e4016da3693c02547c9446303c84f9e
Will MBP <wkerswell@gmail.com>
2014-02-13 18:45:10 +0000

    Added favicon Added example of how to use

--- lib/views/header.php
+++ lib/views/header.php
@@ -6,6 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="description" content="">
 	<meta name="author" content="">
+
+	<link href="favicon.ico" rel="icon" type="image/x-icon" />
+
 	<style>
 		 .unit:after, .group:after {
 			content:".";
--- lib/views/welcome.php
+++ lib/views/welcome.php
@@ -7,14 +7,14 @@
 			<p title="It fits a gap in the market. a shibe one. ">
 				<strong>A quick and simple service for getting pictures of doge for use as placeholders in your designs or code.</strong> Just put your image size (width <span class="amp">&amp;</span> height) after our URL and you'll get a&nbsp;placeholder.
 			</p>
-			<p class="links">
-				blahh blahh blahh <br>
-				blahh...
-			</p>
+				<p class="links">
+					Like this: <a href="/200/300">http://www.place-doge.com/200/300</a><br>
+					or: <a href="/i/200/300">http://www.place-doge.com/i/200/300</a>
+				</p>
 			<img id="image-0" src="i/408/287" alt=""/>
 		</div>
 		<div id="unit-many-many-pictures-of-small-pies" class="unit">
-			<img id="image-1" src="i/200/287?1" alt=""/>
+			<img id="image-1" src="/i/200/287" alt=""/>
 			<img id="image-2" src="i/200/140?2" alt=""/>
 			<img id="image-3" src="i/200/139?3" alt=""/>
 			<img id="image-4" src="i/200/286?4" alt=""/>
